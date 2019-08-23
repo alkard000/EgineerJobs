7428d7cea20adcd1358e61227222098c546e4522
Alkard000 <aburtoivan620@gmail.com>
2019-08-23 13:19:49 -0400

    Creando el Servidor en el Puerto 3000

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/', (req, res) => {
+    res.send('Funciona');
+})
+
+app.listen(3000, () => {
+    console.log('App listening on port 3000!');
+});
