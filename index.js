fdffc69492ec33862930668676386d2055dee859
Alkard000 <aburtoivan620@gmail.com>
2019-08-23 14:54:43 -0400

    Aniadiendo Rutas al Proyecto

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const router = require('./routes');
 
 const app = express();
 
-app.use('/', (req, res) => {
-    res.send('Funciona');
-})
+app.use('/', router());
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
